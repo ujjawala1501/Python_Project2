884935753a3fe2bbf288e3a9b272c80cb789670f
[Ujjawala Katakwar] <ujjawala1501@gmail.com>
2024-09-21 23:03:04 +0530

    pre-finale

--- Hangman/Hangmangame.py
+++ Hangman/Hangmangame.py
@@ -9,22 +9,28 @@ len_s = len(s)
 word=[]
 cntr=1
 list_s = list(s)
+hangman_figure_cntr = 0
 
 for i in range(0,len(s)):
     word.append("_")
 
+
 print(s)
-print("You have 8 chances to guess the word")
-while cntr <= 8:
+print("You have 7 chances to guess the word")
+while cntr <= 7:
     uinp=input("Enter a letter : ")
    # for i in range(0,len(word)): # word has same number of dashes as string
     if uinp in s:
         index_s = list_s.index(uinp)
         word[index_s]=uinp
         print(word)
-        if word.join()
+        final_word = ''.join(word)
+        if final_word == s:
+            print("you fucking won chhoti")
+            break
     else:
-        print(lives_visual_dict[cntr])
+        hangman_figure_cntr += 1
+        print(lives_visual_dict[hangman_figure_cntr])
         print(word)
     cntr+=1
 
